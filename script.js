66dcbf53971a5922e555ff9c6066942785751555
lolio9 <87971315+lolio9@users.noreply.github.com>
2024-11-12 09:23:48 +0100

    .

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ const animatedElements = document.querySelectorAll('.animate');
 function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom >= 0
     );
 }
 
